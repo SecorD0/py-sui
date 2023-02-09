42fa0214bbd54eebe8f0cfa6d1c160f5c1827f85
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2023-02-09 17:19:48 +0300

    Fix bugs

---
+++ py_sui/types.py
@@ -0,0 +1,18 @@
+from typing import Union
+
+Base64 = str
+EventFilter = str
+EventID = str
+EventQuery = dict
+ExecuteTransactionRequestType = str
+ObjectID = str
+RPCTransactionRequestParams = dict
+SequenceNumber = int
+SignatureScheme = str
+SuiAddress = str
+SuiJsonValue = Union[dict, list]
+TransactionDigest = str
+TransactionQuery = dict
+TxBytes = bytes
+TxHash = bytes
+TypeTag = str
--- py_sui/wallet.py
+++ py_sui/wallet.py
@@ -23,49 +23,50 @@ class Wallet:
                 address = self.client.account.address
 
             response = RPC.getObjectsOwnedByAddress(client=self.client, address=address)
-            queries = [RPC.getObject(client=self.client, object_id=obj['objectId'], get_json=True) for obj in
-                       response['result']]
-            query_list = split_list(queries, 200)
-            objs = []
-            for json_data in query_list:
-                objs += RPC.send_request(client=self.client, json_data=json_data)
+            if response['result']:
+                queries = [RPC.getObject(client=self.client, object_id=obj['objectId'], get_json=True) for obj in
+                           response['result']]
+                query_list = split_list(queries, 200)
+                objs = []
+                for json_data in query_list:
+                    objs += RPC.send_request(client=self.client, json_data=json_data)
 
-            for obj in objs:
-                obj_id = obj['result']['details']['reference']['objectId']
-                obj_data = obj['result']['details']['data']
-                obj_type = parse_type(obj_data['type'])
-                obj_fields = obj_data['fields']
-                if obj_type.module == 'coin':
-                    obj_balance = int(obj_fields['balance'])
-                    obj_id = ObjectID(id=obj_id, amount=obj_balance)
-                    if obj_type.structure.name == 'sui':
-                        if balance.coin:
-                            balance.coin.balance += obj_balance
-                            balance.coin.object_ids.append(obj_id)
+                for obj in objs:
+                    obj_id = obj['result']['details']['reference']['objectId']
+                    obj_data = obj['result']['details']['data']
+                    obj_type = parse_type(obj_data['type'])
+                    obj_fields = obj_data['fields']
+                    if obj_type.module == 'coin':
+                        obj_balance = int(obj_fields['balance'])
+                        obj_id = ObjectID(id=obj_id, amount=obj_balance)
+                        if obj_type.structure.name == 'sui':
+                            if balance.coin:
+                                balance.coin.balance += obj_balance
+                                balance.coin.object_ids.append(obj_id)
+
+                            else:
+                                balance.coin = Coin(name=obj_type.structure.name, symbol=obj_type.structure.symbol,
+                                                    package_id=obj_type.structure.package_id,
+                                                    balance=obj_balance, object_ids=[obj_id])
 
                         else:
-                            balance.coin = Coin(name=obj_type.structure.name, symbol=obj_type.structure.symbol,
-                                                package_id=obj_type.structure.package_id,
-                                                balance=obj_balance, object_ids=[obj_id])
+                            if obj_type.structure.name in balance.tokens:
+                                coin = balance.tokens[obj_type.structure.name]
+                                coin.balance += obj_balance
+                                coin.object_ids.append(obj_id)
+
+                            else:
+                                balance.tokens[obj_type.structure.name] = Coin(name=obj_type.structure.name,
+                                                                               symbol=obj_type.structure.symbol,
+                                                                               package_id=obj_type.structure.package_id,
+                                                                               balance=obj_balance, object_ids=[obj_id])
+
+                    elif obj_type.module == 'devnet_nft':
+                        balance.nfts[obj_id] = Nft(name=obj_fields['name'], description=obj_fields['description'],
+                                                   image_url=obj_fields['url'], object_id=obj_id)
 
                     else:
-                        if obj_type.structure.name in balance.tokens:
-                            coin = balance.tokens[obj_type.structure.name]
-                            coin.balance += obj_balance
-                            coin.object_ids.append(obj_id)
-
-                        else:
-                            balance.tokens[obj_type.structure.name] = Coin(name=obj_type.structure.name,
-                                                                           symbol=obj_type.structure.symbol,
-                                                                           package_id=obj_type.structure.package_id,
-                                                                           balance=obj_balance, object_ids=[obj_id])
-
-                elif obj_type.module == 'devnet_nft':
-                    balance.nfts[obj_id] = Nft(name=obj_fields['name'], description=obj_fields['description'],
-                                               image_url=obj_fields['url'], object_id=obj_id)
-
-                else:
-                    balance.misc[obj_id] = obj_data
+                        balance.misc[obj_id] = obj_data
 
         except:
             logging.exception('balance')
@@ -78,31 +79,39 @@ class Wallet:
         if not balance:
             coin = None
             response = RPC.getObjectsOwnedByAddress(client=self.client, address=self.client.account.address)
-            queries = [RPC.getObject(client=self.client, object_id=obj['objectId'], get_json=True) for obj in
-                       response['result']]
-            query_list = split_list(queries, 200)
-            objs = []
-            for json_data in query_list:
-                objs += RPC.send_request(client=self.client, json_data=json_data)
+            if response['result']:
+                queries = [RPC.getObject(client=self.client, object_id=obj['objectId'], get_json=True) for obj in
+                           response['result']]
+                query_list = split_list(queries, 200)
+                objs = []
+                for json_data in query_list:
+                    objs += RPC.send_request(client=self.client, json_data=json_data)
 
-            for obj in objs:
-                obj_id = obj['result']['details']['reference']['objectId']
-                obj_data = obj['result']['details']['data']
-                obj_type = parse_type(obj_data['type'])
-                obj_fields = obj_data['fields']
-                if obj_type.module == 'coin' and obj_type.structure.name == 'sui':
-                    obj_balance = int(obj_fields['balance'])
-                    obj_id = ObjectID(id=obj_id, amount=obj_balance)
-                    if coin:
-                        coin.balance += obj_balance
-                        coin.object_ids.append(obj_id)
+                for obj in objs:
+                    obj_id = obj['result']['details']['reference']['objectId']
+                    obj_data = obj['result']['details']['data']
+                    obj_type = parse_type(obj_data['type'])
+                    obj_fields = obj_data['fields']
+                    if obj_type.module == 'coin' and obj_type.structure.name == 'sui':
+                        obj_balance = int(obj_fields['balance'])
+                        obj_id = ObjectID(id=obj_id, amount=obj_balance)
+                        if coin:
+                            coin.balance += obj_balance
+                            coin.object_ids.append(obj_id)
 
-                    else:
-                        coin = Coin(name=obj_type.structure.name, symbol=obj_type.structure.symbol,
-                                    package_id=obj_type.structure.package_id, balance=obj_balance, object_ids=[obj_id])
+                        else:
+                            coin = Coin(name=obj_type.structure.name, symbol=obj_type.structure.symbol,
+                                        package_id=obj_type.structure.package_id, balance=obj_balance,
+                                        object_ids=[obj_id])
+
+            else:
+                coin = None
 
         else:
             coin = balance.coin
+
+        if not coin:
+            raise exceptions.InsufficientBalance()
 
         sorted_objects = sorted(coin.object_ids, key=lambda obj: obj.amount, reverse=reverse)
         for object_id in sorted_objects:
